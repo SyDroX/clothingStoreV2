6386fddb104520308c1b4c558746409acaaaa381
Kostya <sydoruk.kostya@gmail.com>
2018-03-13 11:56:39 +0200

    got it

--- appController.js
+++ appController.js
@@ -6,8 +6,8 @@ var derp = [
     { "id": 1, "price": "50$", "imageUrl": "images/pp.jpg" },
     { "id": 2, "price": "50$", "imageUrl": "images/pp.jpg" },
     { "id": 3, "price": "50$", "imageUrl": "images/pp.jpg" },
-    { "id": 4, "price": "50$", "imageUrl": "images/pp.jpg" },
-    { "id": 4, "price": "50$", "imageUrl": "images/pp.jpg" }
+    { "id": 4, "price": "50$", "imageUrl": "images/derp.png" },
+    { "id": 5, "price": "50$", "imageUrl": "images/derp.png" }
 ];
 
 var app = angular.module('myApp', ['ngMaterial']);
