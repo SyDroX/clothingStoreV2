f956ed788ddcd1ce201512a0fd66c9bc35680f02
jezuzcrist <vfxlord@gmail.com>
2018-03-14 18:11:05 +0200

    working and saving data to file

--- appController.js
+++ appController.js
@@ -11,17 +11,30 @@ var derp = [
 ];
 
 var app = angular.module('myApp', ['ngMaterial']);
-app.controller('myCtrl', function($scope) {
+app.controller('myCtrl', function ($scope) {
     $scope.products = derp;
     $scope.cart = [];
     $scope.total = 0;
     $scope.startTime = new Date();
     $scope.addToCart = function (price) {
-        $scope.total+= price;
+        $scope.total += price;
     };
 
     $scope.checkout = function (price) {
         var time = (new Date() - $scope.startTime) / 1000;
-        alert("total:" + $scope.total + " time:" + time);
+        // alert("total:" + $scope.total + " time:" + time);
+
+        var url = '/save';
+        var data = { money: $scope.total, timeSpentInShop: time }
+
+        fetch(url, {
+            method: 'POST', // or 'PUT'
+            body: JSON.stringify(data),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        }).then(res => res.json())
+            .catch(error => console.error('Error:', error))
+            .then(response => console.log('Success:', response));
     };
 });
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+var fs = require('fs')
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(5000, () => console.log('Example app listening on port 5000!'))
+
+app.use('/', express.static('./'))
+
+var data = []
+
+function saveToFile() {
+    fs.writeFile(`${__dirname}/data.json`, JSON.stringify(data), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!")
+    });
+}
+
+app.post('/save', (req, res) => {
+    let dataToAdd = req.body
+    data.push(dataToAdd)
+    saveToFile()
+    res.json({})
+})
